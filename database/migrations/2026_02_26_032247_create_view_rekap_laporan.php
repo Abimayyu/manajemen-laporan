0842e1be7c8a55080b2cb91f339b2688443ac370
Abimayyu <abimayyu26@gmail.com>
2026-02-26 10:27:14 +0700

    feat view rekap laporan

---
+++ database/migrations/2026_02_26_032247_create_view_rekap_laporan.php
@@ -0,0 +1,35 @@
+<?php
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+         DB::statement("
+            CREATE VIEW view_rekap_laporan AS
+            SELECT 
+                MONTH(tanggal_laporan) AS bulan,
+                YEAR(tanggal_laporan) AS tahun,
+                COUNT(*) AS jumlah_laporan_masuk,
+                SUM(CASE WHEN status = 'diproses' THEN 1 ELSE 0 END) AS jumlah_diproses,
+                SUM(CASE WHEN status = 'selesai' THEN 1 ELSE 0 END) AS jumlah_selesai,
+                SUM(CASE WHEN status = 'ditolak' THEN 1 ELSE 0 END) AS jumlah_ditolak
+            FROM laporan
+            GROUP BY YEAR(tanggal_laporan), MONTH(tanggal_laporan)
+        ");
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('view_rekap_laporan');
+    }
+};
